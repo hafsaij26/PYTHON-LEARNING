670600629583f6e552ac6286d4960bcd5731fdff
HAFSA IJAZ <hafsaijaz0206@gmail.com>
2026-06-24 00:12:55 +0500

    BASIC CODE

---
+++ START.py
@@ -0,0 +1,14 @@
+# started functions
+print ("hello world")
+#VARIABLES
+name = "HAFSA"
+age = 21
+is_student = True
+print (name)
+print (age)
+print (is_student)
+# end functions
+# DATATYPES
+print (type(name))
+print (type(age))
+print (type(is_student))
