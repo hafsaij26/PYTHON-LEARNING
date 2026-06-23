ea3dd81619ef89733549b9ba6f9cb8a54d19d31e
HAFSA IJAZ <hafsaijaz0206@gmail.com>
2026-06-24 00:26:29 +0500

    added calulations

--- START.py
+++ START.py
@@ -12,3 +12,10 @@ print (is_student)
 print (type(name))
 print (type(age))
 print (type(is_student))
+#input function
+name = input("what is your name? ")
+print (name)
+# Calculation
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+print ("SUM IS ", num1 + num2)
