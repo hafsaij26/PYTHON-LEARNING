a1f8af1e653799821fbfbb9617a7f3af6b29d272
HAFSA IJAZ <hafsaijaz0206@gmail.com>
2026-07-21 23:43:05 +0500

    returning a value

--- functions.py
+++ functions.py
@@ -5,8 +5,16 @@ def greet():
 greet()
 greet()
 greet()
-
+#parameter
 def greet(name):
     print("Hello", name)
 
 greet("hafsa")
+
+#Function Returning a Value
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+result = calculate_area(5, 3)
+print("The area of the rectangle is:", result)
