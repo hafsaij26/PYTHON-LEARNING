d31f00b8ca59e80e8f01a8db8f39a2512ea7383d
HAFSA IJAZ <hafsaijaz0206@gmail.com>
2026-07-28 02:19:07 +0500

    print

--- functions.py
+++ functions.py
@@ -81,3 +81,7 @@ for row in range(1, 6):
     for col in range(1, row + 1):
         print("*", end=" ")
     print()
+for row in range(5, 0, -1):
+    for col in range(1, row + 1):
+        print("*", end=" ")
+    print()
