58a5eced258c28c52580d5091c74e0bc3ee8c43d
HAFSA IJAZ <hafsaijaz0206@gmail.com>
2026-07-27 23:32:24 +0500

    * season

--- functions.py
+++ functions.py
@@ -76,3 +76,8 @@ for row in range(5):
     for col in range(5):
         print("*", end="")
     print()
+
+for row in range(1, 6):
+    for col in range(1, row + 1):
+        print("*", end=" ")
+    print()
