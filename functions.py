431a7b63873c587dec6de1ab94e6b11b64d26e3e
HAFSA IJAZ <hafsaijaz0206@gmail.com>
2026-08-05 23:58:43 +0500

    another

--- functions.py
+++ functions.py
@@ -89,3 +89,9 @@ for i in range(5, 0, -1):
     for j in range(i):
         print("*", end="")
     print()
+for i in range(7,0,-1):
+    for j in range(7-i):
+        print(" ", end="")
+    for k in range(2*i-1):
+        print("*", end="")
+    print()
