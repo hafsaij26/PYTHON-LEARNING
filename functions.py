32a833eb664481a83fa838935bae1af90a409a10
HAFSA IJAZ <hafsaijaz0206@gmail.com>
2026-08-06 00:09:37 +0500

    daimond 1sthalf

--- functions.py
+++ functions.py
@@ -102,3 +102,10 @@ for i in range(1, 6):
     for k in range(2 * i - 1):
         print("*", end="")
     print()
+//daimond
+for i in range(1, 9):
+    for j in range(8 - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()
