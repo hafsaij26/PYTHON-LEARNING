63bc152c2b6ca024b7c5fe812123efe4f092ea06
HAFSA IJAZ <hafsaijaz0206@gmail.com>
2026-08-06 00:02:22 +0500

    right triangle

--- functions.py
+++ functions.py
@@ -89,9 +89,16 @@ for i in range(5, 0, -1):
     for j in range(i):
         print("*", end="")
     print()
-for i in range(7,0,-1):
+for i in range(,0,-1):
     for j in range(7-i):
         print(" ", end="")
     for k in range(2*i-1):
+        print("*", end="")
+    print()
+
+for i in range(1, 6):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
         print("*", end="")
     print()
