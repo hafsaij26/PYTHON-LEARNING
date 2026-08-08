e267ab4609cc5d7c50957f4314473f253df6fe31
HAFSA IJAZ <hafsaijaz0206@gmail.com>
2026-08-08 23:41:01 +0500

    Implement Floyd's Triangle in rowcol.py
    Added implementation for Floyd's Triangle.

--- rowcol.py
+++ rowcol.py
@@ -12,3 +12,11 @@ for i in range(5, 0, -1):
     for j in range(1, i + 1):
         print(j, end=" ")
     print()
+#Floyd's Triangle
+num = 1
+
+for i in range(1, 6):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
